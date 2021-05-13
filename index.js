e50e64d499bf38aee70111b79ccf6dd27e2d5e0e
lycho33 <cho.lyds@gmail.com>
2021-05-13 15:35:58 -0400

    commit tree 79e2a8f425ca6e46ad122c0b58ee4989801ebd66

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Add your code here
+//   fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json", //what type of data we're sending and where does it come from
+//     "Accept": "application/json" //both are headers
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   })
+// }); 
+
+//OR DO THIS. IT'S THE SAME
+
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+  
+//   let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configObj)
+//   .then(function(response) {
+//     return response.json(); //json is available
+//   })
+//   .then(function(object) {
+//     console.log(object); //data is returned as JS Object
+//   })
+//   .catch(function(error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   }); 
+
+  
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
